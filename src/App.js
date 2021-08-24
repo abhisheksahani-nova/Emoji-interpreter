2819054f3b6f6ce73fc2e6a07f2749a69b6b149e
abhisheksahani-nova <75006042+abhisheksahani-nova@users.noreply.github.com>
2021-08-24 14:06:39 +0530

    feat

--- src/App.js
+++ src/App.js
@@ -11,13 +11,23 @@ var emojiDictionary = {
 };
 
 export default function App() {
-  const [meaning, setMeaning] = useState("");
+  const [meaning, setMeaning] = useState("translation will appear here..");
+
+  function changeHandler(event) {
+    var emojiMeaning = emojiDictionary[event.target.value];
+    console.log(emojiMeaning);
+    if (emojiMeaning) {
+      setMeaning(emojiMeaning);
+    } else {
+      setMeaning("failure to recognise this emoji");
+    }
+  }
 
   return (
     <div className="App">
       <h1>Inside out</h1>
-      <input style={{}} placeholder="Search your emoji" />
-      <div> {meaning} </div>
+      <input onChange={changeHandler} placeholder="Search your emoji" />
+      <div style={{ paddingTop: "15px", paddingBottom: "20px" }}>{meaning}</div>
     </div>
   );
 }
